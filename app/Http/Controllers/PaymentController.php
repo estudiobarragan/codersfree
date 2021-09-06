40711a701a1705822998a2e436d7027f7dbc8925
Jose Maria <estudiobarragan0@gmail.com>
2021-09-05 21:03:50 -0300

    Se modifico SDK 1 a V2 de Paypal

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,105 +6,28 @@ use App\Models\Course;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
-use PayPal\Api\Amount;
-use PayPal\Api\Payer;
-use PayPal\Api\Payment;
-use PayPal\Api\PaymentExecution;
-use PayPal\Api\RedirectUrls;
-use PayPal\Api\Transaction;
-use PayPal\Auth\OAuthTokenCredential;
-use PayPal\Exception\PayPalConnectionException;
-use PayPal\Rest\ApiContext;
 use PhpParser\Node\Stmt\TryCatch;
 
 /* require __DIR__  . '/vendor/autoload.php'; */
 
 class PaymentController extends Controller
 {
-  private $apiContext;
-
-  public function __construct()
-  {
-
-    // After Step 1
-    $this->apiContext = new ApiContext(
-      new \PayPal\Auth\OAuthTokenCredential(
-        config('paypal.paypal.client_id'),     // ClientID
-        config('paypal.paypal.client_secret')      // ClientSecret
-      )
-    );
-  }
 
   public function checkout(Course $course)
   {
-    return view('payment.checkout', compact('course'));
+    $header = config('paypal.paypal.client_id');
+    return view('payment.checkout', compact('course', 'header'));
   }
   public function pay(Course $course)
   {
-
-    // After Step 2
-    $payer = new Payer();
-    $payer->setPaymentMethod('paypal');
-
-    $amount = new Amount();
-    $amount->setTotal($course->price->value);
-    $amount->setCurrency('USD');
-
-    $transaction = new Transaction();
-    $transaction->setAmount($amount);
-    // $transaction->setDescription('Estas por comprar el curso: ' . $course->title);
-
-    $redirectUrls = new RedirectUrls();
-    $redirectUrls->setReturnUrl(route('payment.approved', $course))
-      ->setCancelUrl(route('payment.checkout', $course));
-
-    $payment = new Payment();
-    $payment->setIntent('sale')
-      ->setPayer($payer)
-      ->setTransactions(array($transaction))
-      ->setRedirectUrls($redirectUrls);
-
-    // After Step 3
-    try {
-      $payment->create($this->apiContext);
-
-      return redirect()->away($payment->getApprovalLink());
-    } catch (PayPalConnectionException $ex) {
-      // This will print the detailed information on the exception.
-      //REALLY HELPFUL FOR DEBUGGING
-      echo $ex->getData();
-    }
   }
 
   public function approved(Request $request, Course $course)
   {
 
-    $paymentId = $_GET['paymentId'];
+    //  return redirect()->route('payment.checkout', $course)->with('failed', 'Su pago no se concreto, por favor, vuelva a intentarlo');
 
+    //  return redirect()->route('courses.status', $course)->with('approved', 'Gracias por su compra, el curso queda habilitado.');
 
-    try {
-      $payment = Payment::get($paymentId, $this->apiContext);
-    } catch (Exception $ex) {
-
-      dd($ex);
-      return redirect()->route('payment.checkout', $course)->with('failed', 'Su pago no se concreto, por favor, vuelva a intentarlo');
-    }
-
-
-    $execution = new PaymentExecution();
-    $execution->setPayerId($_GET['PayerID']);
-
-    try {
-      $result = $payment->execute($execution, $this->apiContext);
-    } catch (Exception $ex) {
-      return redirect()->route('payment.checkout', $course)->with('failed', 'Su pago no se concreto, por favor, vuelva a intentarlo');
-    }
-
-    if ($result->getState() === 'approved') {
-      $course->students()->attach(auth()->user()->id);
-      return redirect()->route('courses.status', $course)->with('approved', 'Gracias por su compra, el curso queda habilitado.');
-    } else {
-      return redirect()->route('payment.checkout', $course)->with('failed', 'Su pago no se concreto, por favor, vuelva a intentarlo');
-    }
   }
 }
--- resources/views/payment/checkout.blade.php
+++ resources/views/payment/checkout.blade.php
@@ -1,6 +1,43 @@
 <x-app-layout>
-  
-  @if(session('failed'))
+  <script src="https://www.paypal.com/sdk/js?client-id=test"></script>
+  <script
+    src="https://www.paypal.com/sdk/js?client-id=".{{$header}}> 
+    // Required. Replace YOUR_CLIENT_ID with your sandbox client ID.
+  </script>
+
+  <script>
+    paypal.Buttons({
+      createOrder: function(data, actions) {
+        // This function sets up the details of the transaction, including the amount and line item details.
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: {{$course->price->value}}
+            }
+          }]
+        });
+      },
+      onApprove: function(data, actions) {
+        // This function captures the funds from the transaction.
+        return actions.order.capture().then(function(details) {
+          // This function shows a transaction success message to your buyer.
+          alert('Transaction completed by ' + details.payer.name.given_name);
+        });
+      },
+      onCancel: function (data) {
+        
+        alert('Transaccion cancelada por el usuraio');
+        return data;
+      },
+      onError: function (err) {
+        // For example, redirect to a specific error page
+        window.location.href = "/your-error-page-here";
+      }
+    }).render('#paypal-button-container');
+    //This function displays Smart Payment Buttons on your web page.
+  </script>
+
+  @isset($fallo)
     <div class="card max-w-4xl mx-auto mt-8 text-center">
       <div class="card-body  bg-red-100 w-full">
         <div class="alert alert-danger bg-red-600 text-white"  role="alert">
@@ -8,7 +45,7 @@
         </div>
       </div>
     </div>
-  @endif
+  @endisset
 
   <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 py-12">
     <h1 class="text-gray-500 text-3xl font-bold">Detalle del pedido</h1>
@@ -24,17 +61,27 @@
           <p class="text-xl font-bold ml-auto">u$s {{$course->price->value}}</p>
         </article>
 
-        <div class="flex justify-end mt-2 mb-4">
+        {{-- <div class="flex justify-end mt-2 mb-4">
           <a class="btn btn-primary" href="{{ route( 'payment.pay', $course) }}">Comprar este curso</a>
-        </div>
-
+        </div> --}}
+        
+        
         <hr>
         <p class="text-sm mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, quasi, placeat maiores nostrum cumque velit molestiae, exercitationem veritatis vitae beatae ad rem? Repellendus impedit minima animi at soluta sapiente maiores?
           <a class="text-red-500 font-bold" href="">Terminos y Condiciones</a>
         </p>
+        <div class="card flex justify-end mt-8">
+          <div class="card-body ">
+
+          <div id="paypal-button-container"></div>
+
+          </div>
+        </div>
 
       </div>
     </div>
   </div>
+
+  
 
 </x-app-layout>

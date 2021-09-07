5347d98dd9f2f8bb59280686d8dc4a55679fe2e9
Jose Maria <estudiobarragan0@gmail.com>
2021-09-07 17:37:01 -0300

    22 Se procura mejorar experiencia en pagos
    Sin éxito se intenta mejorar la recuperación de datos del proceso de pagos con paypal. Sin exito

--- resources/views/payment/checkout.blade.php
+++ resources/views/payment/checkout.blade.php
@@ -9,7 +9,8 @@
         return actions.order.create({
           purchase_units: [{
             amount: {
-              value: {{$course->price->value}}
+              value: {{$course->price->value}},
+              currency_code: "USD",
             }
           }]
         });
@@ -18,20 +19,37 @@
         // This function captures the funds from the transaction.
         return actions.order.capture().then(function(details) {
           // This function shows a transaction success message to your buyer.
-          alert('Transaction completed by ' + details.payer.name.given_name);
-          var id = {{$course->id}};
-          window.location.href = "/payment/approved/"+id;
-        });
+          /* console.log(details.payer);
+          alert('Transaction completed by ' + details.payer.name.given_name); */
+          /* var id = {{$course->id}};
+          window.location.href = "/payment/approved/"+id; */
+          
+          /* console.log('Capture result', details, JSON.stringify(details, null, 2));
+          console.log('Payer: ', JSON.stringify(payer, null, 2)); */
+          
+          var transaction = details.purchase_units[0].payments.captures[0];    
+          var payer = details.payer;
+          var courseId= {{$course->id}};
+          window.location.href = "/payment/approved/"+{{$course->id}}+"/"+JSON.stringify(transaction, null, 2);
+          /* var req = new XMLHttpRequest();          
+          var token = document.querySelector('meta[name="csrf-token"]').content;
+          req.open("POST", "/payment/otherapproved", true);
+          req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          req.setRequestHeader("x-csrf-token", token);
+          req.setRequestHeader("Accept", "application/json");
+          req.send(JSON.stringify(details));
+          return details; */
+        })
       },
       onCancel: function (data) {
-        var id = {{$course->id}};
-        window.location.href = "/payment/failed/"+id;
+          window.location.href = "/payment/failed/"+{{$course->id}}+"/"+JSON.stringify(data);
         return data;
-      }/* ,
-      onError: function (err) {
-        // For example, redirect to a specific error page
-        window.location.href = "/payment/failed";
-      } */
+        },
+        /* ,
+        onError: function (err) {
+          // For example, redirect to a specific error page
+          window.location.href = "/payment/failed";
+        } */
     }).render('#paypal-button-container');
     //This function displays Smart Payment Buttons on your web page.
   </script>
@@ -45,7 +63,7 @@
       </div>
     </div>
   @endif
-
+  
   <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 py-12">
     <h1 class="text-gray-500 text-3xl font-bold">Detalle del pedido</h1>
 
@@ -55,7 +73,7 @@
         <article class="flex items-center">
           <img class="h-12 w-12 object-cover" src="{{Storage::url($course->image->url)}}" alt="">
 
-          <h1 class="text-lg ml-2">{{$course->title}}</h1>
+          <a href="{{route('courses.show',$course)}}" class="text-lg ml-2 hover:bg-gray-100" >{{$course->title}}</a>
 
           <p class="text-xl font-bold ml-auto">u$s {{$course->price->value}}</p>
         </article>
@@ -65,14 +83,15 @@
         </div> --}}
         
         
-        <hr>
+        <hr class="mt-4">
         <p class="text-sm mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, quasi, placeat maiores nostrum cumque velit molestiae, exercitationem veritatis vitae beatae ad rem? Repellendus impedit minima animi at soluta sapiente maiores?
           <a class="text-red-500 font-bold" href="">Terminos y Condiciones</a>
         </p>
         <div class="card flex justify-end mt-8">
           <div class="card-body ">
-
-          <div id="paypal-button-container"></div>
+            
+            {{-- {{$token = csrf_field()}} --}}
+            <div id="paypal-button-container"></div>
 
           </div>
         </div>
